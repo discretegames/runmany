1c56615a886a56d56b802b97529438c77b04958e
discretegames <discretizedgames@gmail.com>
2021-08-22 14:46:35 -0700

    Restructured again. Realized this works best.

--- src/__init__.py
+++
@@ -1,7 +0,0 @@
-"""TODO docstrings."""
-
-from .run_many import runmany
-from .run_many import runmanys
-from .run_many import runmanyf
-
-__all__ = ['runmany', 'runmanys', 'runmanyf']
---
+++ src/run_many/__init__.py
@@ -0,0 +1,7 @@
+"""TODO docstrings."""
+
+from .runmany import runmany
+from .runmany import runmanys
+from .runmany import runmanyf
+
+__all__ = ['runmany', 'runmanys', 'runmanyf']
