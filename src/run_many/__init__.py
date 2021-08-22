9ed41064201bb918ab54924b3fcfd1f9fa7b8651
discretegames <discretizedgames@gmail.com>
2021-08-22 12:08:35 -0700

    Testing __all__.

--- src/run_many/__init__.py
+++ src/run_many/__init__.py
@@ -1,6 +1,6 @@
 """TODO docstrings."""
 
-#__all__ = ['runmany', 'runmanys', 'runmanyf']
 from .run_many import runmany
 from .run_many import runmanys
 from .run_many import runmanyf
+__all__ = ['runmany', 'runmanys', 'runmanyf']
