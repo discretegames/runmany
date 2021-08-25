f898ad231ba9ad8b64d1f1e8aa971650643e2aff
discretegames <discretizedgames@gmail.com>
2021-08-24 23:25:24 -0700

    Added scaffolding for all other tests to do.

---
+++ tests/run_many/test_syntax.py
@@ -0,0 +1,38 @@
+def test_all_expands() -> None:
+    assert 0
+
+
+def test_section_header() -> None:
+    assert 0
+
+
+def test_code_list() -> None:
+    assert 0
+
+
+def test_argv_overwrite() -> None:
+    assert 0
+
+
+def test_argv_list() -> None:
+    assert 0
+
+
+def test_stdin_overwrite() -> None:
+    assert 0
+
+
+def test_stdin_list() -> None:
+    assert 0
+
+
+def test_comments() -> None:
+    assert 0
+
+
+def test_exit() -> None:
+    assert 0
+
+
+def test_similar_syntax() -> None:
+    assert 0
