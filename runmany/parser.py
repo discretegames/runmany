c4af5f301a0da71d69061f154d33d18dd09a6a53
discretegames <discretizedgames@gmail.com>
2021-09-09 01:53:22 -0700

    Rewrote parser and section iterator.

--- runmany/parser.py
+++ runmany/parser.py
@@ -2,123 +2,109 @@ import re
 import enum
 from typing import List, Optional, Tuple, Generator, Union, TextIO, cast
 
+from dataclasses import dataclass
+
 from runmany.settings import Settings
 from runmany.util import removeprefix, removesuffix, print_err
 
 
 class Syntax:
-    ARGV_KEYWORD = "Argv for"  # todo ensure whitespace after for
-    STDIN_KEYWORD = "Stdin for"
-    ALSO_KEYWORD = "Also"
-    EXIT_KEYWORD = "Exit."
-    HEADER_SUFFIX = ':'
-
-    LANGUAGE_DIVIDER = ','
-    DISABLE_PREFIX = '!'
-    INLINE_COMMENT = '%'
+    ARGV_ALL = "Argv"
+    ARGV_FOR = f"{ARGV_ALL} for"
+    STDIN_ALL = "Stdin"
+    STDIN_FOR = f"{STDIN_ALL} for"
+    ALSO = "Also"
+    DISABLER = '!'
+    HEADER_END = ':'
+    SEPARATOR = ','
+    COMMENT = '%'
+    EXIT = "Exit."
     TAB_INDENT = '\t'
     SPACE_INDENT = '    '
+    PATTERN1 = f'(?=\S)((?:{DISABLER})?)((?:{ARGV_ALL})|(?:{STDIN_ALL})|(?:{ALSO}))\s*(?:{HEADER_END})\s*(.*)'
+    PATTERN2 = f'(?=\S)((?:{DISABLER})?)((?:{ARGV_FOR}\s)|(?:{STDIN_FOR}\s)|)([^{HEADER_END}]*?)(?:{HEADER_END})\s*(.*)'
     # todo block comments?
 
 
-class SectionType(enum.Enum):  # todo rename back to section type
+class SectionType(enum.Enum):
     CODE = enum.auto()
     ARGV = enum.auto()
     STDIN = enum.auto()
     UNKNOWN = enum.auto()
 
 
+@dataclass
 class Section:
-    def __init__(
-            self, header: str, header_type: SectionType, line_number: int, content: str, settings: Settings) -> None:
-        self.header = header
-        self.header_type = header_type
-        self.line_number = line_number
+    type: SectionType
+    is_disabled: bool
+    is_also: bool
+    line_number: int
+    languages: List[str]
+    content: str = ''
 
-        self.is_disabled = self.header.startswith(Syntax.DISABLE_PREFIX)
-        self.is_also = get_header_type(self.header) is SectionType.UNKNOWN
-        self.has_content = bool(content.strip('\r\n'))  # todo combine with .content later, need to change set_content
-        self.init_content(content)
-        self.init_languages(settings)
+    @staticmethod
+    def try_start_section(line: str, line_number: int, settings: Settings) -> Optional[Tuple['Section', str]]:
+        if Syntax.HEADER_END not in line:
+            return None
 
-        self.languages = get_header_languages(header, settings)
-        if self.is_also:
-            return
+        match = re.fullmatch(Syntax.PATTERN1, line)
+        if match:  # Matched "Argv:", "Stdin:", or "Also:" style header.
+            disabler, keyword, top_line = match.groups()
+            if keyword == Syntax.ALSO:
+                section_type = SectionType.UNKNOWN
+                is_also = True
+                languages = []
+            else:
+                section_type = SectionType.ARGV if keyword == Syntax.ARGV_ALL else SectionType.STDIN
+                is_also = False
+                languages = settings.all_languages()
+            return Section(section_type, bool(disabler), is_also, line_number, languages), top_line
 
-        raw_header = get_plain_header(self.header)
-        if not self.is_also:
-            raw_header = removesuffix(removeprefix(raw_header, start), end)
-            for language in raw_header.split(Syntax.LANGUAGE_DIVIDER):
-                try:
-                    self.languages.extend(settings.unpack(language))  # todo empty is all now
-                except KeyError:
-                    if not self.is_disabled:
-                        print_err(f'Language "{language.strip()}" in section header at line {self.line_number}'
-                                  ' not found in json. Skipping language.')
+        match = re.fullmatch(Syntax.PATTERN2, line)
+        if match:  # Matched "Argv for Lang1, Lang2:" or "Stdin for Lang1, Lang2:" style header.
+            disabler, keyword, langs, top_line = match.groups()
+            languages = [language.strip() for language in langs.split(Syntax.SEPARATOR)]
+            if not keyword:
+                section_type = SectionType.CODE
+            else:
+                section_type = SectionType.ARGV if keyword.rstrip() == Syntax.ARGV_FOR else SectionType.STDIN
+            return Section(section_type, bool(disabler), False, line_number, languages), top_line
 
-    def init_content(self, content: str) -> None:
-        if self.header_type is SectionType.ARGV:
+        return None
+
+    def set_content(self, content: str) -> None:
+        if self.type is SectionType.ARGV:
             self.content = content.strip('\r\n')  # Always strip argv.
-        elif self.header_type is SectionType.STDIN:
+        elif self.type is SectionType.STDIN:
             self.content = content.strip('\r\n') + '\n'  # Always strip stdin except trailing newline.
         else:
             self.content = content  # Never strip code.
+        self.has_content = bool(content.strip('\r\n'))  # todo combine with .content later
 
-    def init_languages(self, settings: Settings) -> None:
-        header = get_plain_header(self.header)
-        pass
-
-    @staticmethod
-    def try_start_section(line: str, settings: Settings) -> Optional[Tuple['Section', str]]:
-        pass
-
-    def set_content(self, content: str):
-        # todo
-        pass
-
-    def update_type(self, section_type: SectionType) -> SectionType:
+    def finish_section(self, lead_section_type: SectionType, content: str) -> SectionType:
         if self.type is SectionType.UNKNOWN:
-            self.type = section_type
+            self.type = lead_section_type
+        self.set_content(content)
         return self.type
 
 
 def line_is_exit(line: str) -> bool:
-    return line.startswith(Syntax.EXIT_KEYWORD)
+    return line.startswith(Syntax.EXIT)
 
 
 def line_is_comment(line: str) -> bool:
-    return line.startswith(Syntax.INLINE_COMMENT)
+    return line.startswith(Syntax.COMMENT)
 
 
 def line_is_indented(line: str) -> bool:
     return line.startswith(Syntax.TAB_INDENT) or line.startswith(Syntax.SPACE_INDENT)
-
-# todo remove
-# def get_plain_header(header: str) -> str:
-#     return removeprefix(header, Syntax.DISABLE_PREFIX)
-
-
-# def get_header_type(header: str, last_header_type: SectionType = SectionType.UNKNOWN) -> SectionType:
-#     header = get_plain_header(header)
-#     if header.startswith(Syntax.ALSO_KEYWORD + Syntax.HEADER_SUFFIX):
-#         return last_header_type
-#     if header.startswith(Syntax.ARGV_KEYWORD):
-#         return SectionType.ARGV
-#     if header.startswith(Syntax.STDIN_KEYWORD):
-#         return SectionType.STDIN
-#     return SectionType.CODE
 
 
 def section_iterator(file: TextIO) -> Generator[Union[str, None, Section], Settings, None]:
     section: Optional[Section] = None
     lead_section_type = SectionType.UNKNOWN
     content: List[str] = []
-
-    def finish_section() -> Section:
-        nonlocal lead_section_type
-        lead_section_type = section.update_type(lead_section_type)
-        section.set_content(''.join(content))
-        return section
+    settings: Settings
 
     for line_number, line in enumerate(file, 1):
         if line_is_exit(line):
@@ -130,7 +116,8 @@ def section_iterator(file: TextIO) -> Generator[Union[str, None, Section], Setti
             content.append(line)
             continue
 
-        next_section = Section.try_start_section(line, settings)
+        # TODO we need settings for all langs but don't yet know what it is, hmm
+        next_section = Section.try_start_section(line, line_number, settings)
         if next_section is None:
             print_err(f'Skipping line {line_number} "{line}" as it is not a section header and not indented.')
             continue
@@ -139,7 +126,8 @@ def section_iterator(file: TextIO) -> Generator[Union[str, None, Section], Setti
             settings = yield ''.join(content)  # Yield JSON string at top. Only happens once.
             yield None  # Extra yield needed to send back to the send from run_iterator.
         else:
-            yield finish_section()
+            lead_section_type = section.finish_section(lead_section_type, ''.join(content))
+            yield section
 
         section, top_line = next_section
         content.clear()
@@ -149,4 +137,5 @@ def section_iterator(file: TextIO) -> Generator[Union[str, None, Section], Setti
         yield ''.join(content)
         yield None
     else:
-        yield finish_section()
+        section.finish_section(lead_section_type, ''.join(content))
+        yield section
