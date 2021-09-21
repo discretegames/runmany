9ccb2053a8a3ee8e4ad47e285682c8818cb109f7
discretegames <discretizedgames@gmail.com>
2021-09-21 12:23:47 -0700

    Added loads more languages with helloworld and argv examples.

--- runpytest.py
+++ runpytest.py
@@ -5,10 +5,10 @@
 from subprocess import run
 
 print('Running Pytest...')
-# result = run('venv/Scripts/python -m pytest')
+result = run('venv/Scripts/python -m pytest')
 # result = run('venv/Scripts/python -m pytest -vv')
 # result = run('venv/Scripts/python -m pytest -m "not slow"')
-result = run('venv/Scripts/python -m pytest -m "not slow" -q')
+# result = run('venv/Scripts/python -m pytest -m "not slow" -q')
 # result = run('venv/Scripts/python -m pytest -m slow')
 # result = run('venv/Scripts/python -m pytest -m "not slow" -vv')
 # result = run('venv/Scripts/python -m pytest -m "not slow" -x -vv')
