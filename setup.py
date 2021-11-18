452c41f8095818a0a9304a90a1db5cbf310d89a2
discretegames <discretizedgames@gmail.com>
2021-11-18 13:23:53 -0800

    Converted setup.py to setup.cfg.

--- setup.py
+++ setup.py
@@ -1,55 +1,7 @@
 
 """Package setup file for the Python 3 runmany package: https://pypi.org/project/runmany"""
 
-from pathlib import Path
-from setuptools import setup, find_packages
-
-VERSION = "2.0.0"
+import setuptools
 
 if __name__ == '__main__':
-    with open(Path(__file__).with_name('README.md'), encoding='utf-8') as file:
-        long_description = file.read()
-
-    setup(
-        name='runmany',
-        version=VERSION,
-        author='discretegames',
-        author_email='discretizedgames@gmail.com',
-        description="A tool to run many programs written in many languages from one file.",
-        long_description=long_description,
-        long_description_content_type="text/markdown",
-        url='https://github.com/discretegames/runmany',
-        packages=find_packages('src'),
-        package_dir={'': 'src'},
-        package_data={'': ['*.json', 'py.typed']},
-        zip_safe=False,
-        license="MIT",
-        keywords=['run', 'execute', 'other languages', 'multiple languages', 'manyfile',
-                  '.many', 'one file', 'programs', 'chrestomathy', 'polyglot'],
-        project_urls={"GitHub": "https://github.com/discretegames/runmany",
-                      "PyPI": "https://pypi.org/project/runmany",
-                      "TestPyPI": "https://test.pypi.org/project/runmany"},
-        python_requires='>=3.6',
-        classifiers=[
-            "Development Status :: 4 - Beta",
-            "Intended Audience :: Education",
-            "Intended Audience :: Developers",
-            "License :: OSI Approved :: MIT License",
-            "Topic :: Software Development :: Interpreters",
-            "Topic :: Software Development :: Compilers",
-            "Topic :: Software Development :: Build Tools",
-            "Topic :: Utilities",
-            "Typing :: Typed",
-            "Programming Language :: Python :: 3",
-            "Programming Language :: Python :: 3.6",
-            "Programming Language :: Python :: 3.7",
-            "Programming Language :: Python :: 3.8",
-            "Programming Language :: Python :: 3.9",
-            "Programming Language :: Python :: 3.10"
-        ],
-        entry_points={
-            'console_scripts': [
-                'runmany = runmany.runmany:main'
-            ]
-        },
-    )
+    setuptools.setup()
