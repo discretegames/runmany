fee693233929129dfa1265992ac1559d2f5e413c
discretegames <discretizedgames@gmail.com>
2021-11-20 21:29:25 -0800

    Simplified set_sections.

--- src/runmany/newparser.py
+++ src/runmany/newparser.py
@@ -32,7 +32,7 @@ class Syntax(ABC):
     SPACE_INDENT_LENGTH = 4
     SPACE_INDENT = SPACE * SPACE_INDENT_LENGTH
 
-    HEADER_START, HEADER_END = '^(?=\\S)(!!|@@)?(!|@)?\\s*', '\\s*:\\s*(.*)$'
+    HEADER_START, HEADER_END = '^(?=\\S)(!!|@@)?(!|@)?\\s*', '\\s*:'
     SETTINGS_HEADER = HEADER_START + '(Settings)' + HEADER_END
     ARGV_HEADER = HEADER_START + 'Argv(?:\\s+for\\b([^:]*))?' + HEADER_END
     STDIN_HEADER = HEADER_START + 'Stdin(?:\\s+for\\b([^:]*))?' + HEADER_END
@@ -41,21 +41,32 @@ class Syntax(ABC):
 
 
 class Snippet:
-    pass
-    # def __init__(self
+    def __init__(self, parser: 'Parser', first_line: int, last_line: int):
+        self.parser = parser
+        self.first_line = first_line
+        self.last_line = last_line
 
 
 class Section(ABC):
-    def __init__(self, lines: List[str], first_line: int, last_line: int):
-        self.lines = lines
+    def __init__(self, parser: 'Parser', first_line: int, last_line: int):
+        self.parser = parser
         self.first_line = first_line
         self.last_line = last_line
-        matches = cast(re.Match[str], self.get_header_match(lines[first_line])).groups()
+        matches = cast(re.Match[str], self.get_header_match(self.parser.lines[first_line])).groups()
         self.is_disabled = matches[0] == Syntax.SECTION_DISABLER
         self.is_solo = matches[0] == Syntax.SECTION_SOLOER
-        self.header_args = matches[2]
-        self.header_content = matches[3]
+        self.header_arg = matches[2]
+        # self.set_snippets()
         # todo solos list of indexes
+
+    # def set_snippets(self) -> None:
+    #     self.snippets: List[Snippet] = []
+
+    #     i = self.first_line + 1
+    #     while i <= self.last_line:
+    #         # if line is Also: then make snippet from
+
+    #     snippet_firsts: List[int] = []
 
     # def create_also
     # self.alsos: Also = []
@@ -92,8 +103,6 @@ class Section(ABC):
 
 
 class SettingsSection(Section):
-    def __init__(self, lines: List[str], first_line: int, last_line: int):
-        super().__init__(lines, first_line, last_line)
 
     @staticmethod
     def get_header_match(line: str) -> Optional[re.Match[str]]:
@@ -139,7 +148,7 @@ class Parser:
         self.clean_lines()
         self.set_sections()
         self.has_solo_sections = any(section.is_solo for section in self.sections)
-        self.has_solo_snippets = any(section.has_solo_snippets for section in self.sections)
+        # self.has_solo_snippets = any(section.has_solo_snippets for section in self.sections)
 
     def get_first_line(self) -> int:
         for i in range(len(self.lines) - 1, -1, -1):
@@ -163,35 +172,32 @@ class Parser:
                     self.lines[i] = line[:index]
 
     def set_sections(self) -> None:
-        section_firsts: List[int] = []
-        section_lasts: List[int] = []
-        section_types: List[Type[Section]] = []
+        self.sections: List[Section] = []
         i, in_section = self.first_line, False
         while i <= self.last_line:
             line = self.lines[i]
             if not in_section:
-                section_type = Section.try_get_section_type(line)
-                if section_type:
+                tried_section_type = Section.try_get_section_type(line)
+                if tried_section_type:
                     in_section = True
-                    section_firsts.append(i)
-                    section_types.append(section_type)
+                    section_first_line = i
+                    section_type = tried_section_type
             else:
                 if i == self.last_line:
-                    section_lasts.append(i)
+                    self.sections.append(section_type(self, section_first_line, i))
                 else:
                     if line.rstrip().startswith(Syntax.END) or Section.try_get_section_type(line):
                         in_section = False
                         i -= 1
-                        section_lasts.append(i)
+                        self.sections.append(section_type(self, section_first_line, i))
             i += 1
-        self.sections = [st(self.lines, sf, sl) for sf, sl, st in zip(section_firsts, section_lasts, section_types)]
 
     def __iter__(self) -> Iterator[Section]:
-        if not self.settings.ignore_solos:
-            if self.has_solo_sections:
-                return iter(section for section in self.sections if section.is_solo)
-            if self.has_solo_snippets:
-                return iter(section for section in self.sections if section.has_solo_snippets)
+        # if not self.settings.ignore_solos:
+        #     if self.has_solo_sections:
+        #         return iter(section for section in self.sections if section.is_solo)
+        #     if self.has_solo_snippets:
+        #         return iter(section for section in self.sections if section.has_solo_snippets)
         return iter(self.sections)
 
     def __str__(self) -> str:
