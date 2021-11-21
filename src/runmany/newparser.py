8fd76fb1ec9b1a0eb4daeb7e11f05f9104db8bee
discretegames <discretizedgames@gmail.com>
2021-11-20 22:59:06 -0800

    Snippets fully created and printed.

--- src/runmany/newparser.py
+++ src/runmany/newparser.py
@@ -2,16 +2,14 @@
 """todo"""
 
 import re
-import os
+from pprint import pformat
 from typing import Iterator, List, Optional, Type, cast
 from abc import ABC, abstractmethod
 from runmany.settings import Settings
 from runmany.util import print_err
 
-# pylint: disable=too-few-public-methods
 
-
-class Syntax(ABC):
+class Syntax(ABC):  # pylint: disable=too-few-public-methods
     SETTINGS = 'Settings'
     ARGV = 'Argv'
     STDIN = 'Stdin'
@@ -33,30 +31,44 @@ class Syntax(ABC):
     SPACE_INDENT_LENGTH = 4
     SPACE_INDENT = SPACE * SPACE_INDENT_LENGTH
 
-    HEADER_START, HEADER_END = '^(?=\\S)(!!|@@)?(!|@)?\\s*', '\\s*:'
+    HEADER_START, HEADER_END = '^(?=\\S)(!!|@@|)?(!|@|)?\\s*', '\\s*:'
     SETTINGS_HEADER = HEADER_START + '(Settings)' + HEADER_END
     ARGV_HEADER = HEADER_START + 'Argv(?:\\s+for\\b([^:]*))?' + HEADER_END
     STDIN_HEADER = HEADER_START + 'Stdin(?:\\s+for\\b([^:]*))?' + HEADER_END
     CODE_HEADER = HEADER_START + '([^:]*)' + HEADER_END
-    ALSO_HEADER = '^(?=\\S)(?:!|@)?\\s*Also' + HEADER_END
+    ALSO_HEADER = '^(?=\\S)(!|@|)?\\s*Also' + HEADER_END
 
 
 class Snippet:
-    def __init__(self, parser: 'Parser', first_line: int, last_line: int):
+    def __init__(self, parser: 'Parser', first_line: int, last_line: int, sd_match: Optional[str]):
         self.parser = parser
         self.first_line = first_line
         self.last_line = last_line
+        if sd_match is None:
+            sd_match = cast(re.Match[str], self.get_also_header_match(parser.lines[self.first_line])).group(1)
+        self.is_disabled = sd_match == Syntax.DISABLER
+        self.is_solo = sd_match == Syntax.SOLOER
+
+    @staticmethod
+    def get_also_header_match(line: str) -> Optional[re.Match[str]]:
+        return re.match(Syntax.ALSO_HEADER, line)
 
     @staticmethod
     def line_is_also_header(line: str) -> bool:
-        return bool(re.match(Syntax.ALSO_HEADER, line))
+        return bool(Snippet.get_also_header_match(line))
 
     @staticmethod
     def line_is_content(line: str) -> bool:
         return line.startswith(Syntax.TAB_INDENT) or line.startswith(Syntax.SPACE_INDENT) or not line.rstrip()
 
+    def __str__(self) -> str:
+        return pformat(self.parser.lines[self.first_line:self.last_line + 1])
 
-class Section(ABC):
+    def __repr__(self) -> str:
+        return str(self)
+
+
+class Section(ABC):  # pylint: disable=too-many-instance-attributes
     def __init__(self, parser: 'Parser', first_line: int, last_line: int):
         self.parser = parser
         self.first_line = first_line
@@ -65,23 +77,26 @@ class Section(ABC):
         self.is_disabled = matches[0] == Syntax.SECTION_DISABLER
         self.is_solo = matches[0] == Syntax.SECTION_SOLOER
         self.header_arg = matches[2]
-        self.make_snippets()
-        # todo solos list of indexes
+        self.make_snippets(matches[1])  # matches[1] is the first snippet's solo/disabled match
+        self.has_solo_snippets = any(snippet.is_solo for snippet in self.snippets)
 
-    def make_snippets(self) -> None:
+    def make_snippets(self, first_sd_match: str) -> None:
+        def add_snippet() -> None:
+            sd_match = None if self.snippets else first_sd_match
+            self.snippets.append(Snippet(self.parser, snippet_first_line, i - 1, sd_match))
         self.snippets: List[Snippet] = []
         snippet_first_line = self.first_line
         i = snippet_first_line + 1
         while i <= self.last_line:
             line = self.parser.lines[i]
             if Snippet.line_is_also_header(line):
-                self.snippets.append(Snippet(self.parser, snippet_first_line, i - 1))
+                add_snippet()
                 snippet_first_line = i
             elif not Snippet.line_is_content(line):
                 self.parser.lines[i] = ''
                 print_err(f'Skipping invalid unindented line {i} "{line}".')
             i += 1
-        self.snippets.append(Snippet(self.parser, snippet_first_line, self.last_line))
+        add_snippet()
 
     @staticmethod
     def try_get_section_type(line: str) -> Optional[Type['Section']]:
@@ -91,7 +106,7 @@ class Section(ABC):
             return ArgvSection
         if StdinSection.get_header_match(line):
             return StdinSection
-        if CodeSection.get_header_match(line):
+        if not Snippet.line_is_also_header(line) and CodeSection.get_header_match(line):
             return CodeSection
         return None
 
@@ -104,8 +119,10 @@ class Section(ABC):
     def run(self) -> None:
         pass
 
+    # __iter__
+
     def __str__(self) -> str:
-        return str((self.__class__.__name__, self.first_line, self.last_line))
+        return pformat((self.__class__.__name__, self.first_line, self.last_line, self.snippets))
 
     def __repr__(self) -> str:
         return str(self)
@@ -157,7 +174,7 @@ class Parser:
         self.clean_lines()
         self.make_sections()
         self.has_solo_sections = any(section.is_solo for section in self.sections)
-        # self.has_solo_snippets = any(section.has_solo_snippets for section in self.sections) # TODO
+        self.has_solo_snippets = any(section.has_solo_snippets for section in self.sections)
 
     def get_first_line(self) -> int:
         for i in range(len(self.lines) - 1, -1, -1):
@@ -200,15 +217,15 @@ class Parser:
             self.sections.append(section_type(self, section_first_line, self.last_line))
 
     def __iter__(self) -> Iterator[Section]:
-        # if not self.settings.ignore_solos: # TODO
-        #     if self.has_solo_sections:
-        #         return iter(section for section in self.sections if section.is_solo)
-        #     if self.has_solo_snippets:
-        #         return iter(section for section in self.sections if section.has_solo_snippets)
+        if not self.settings.ignore_solos:
+            if self.has_solo_sections:
+                return iter(section for section in self.sections if section.is_solo)
+            if self.has_solo_snippets:
+                return iter(section for section in self.sections if section.has_solo_snippets)
         return iter(self.sections)
 
     def __str__(self) -> str:
-        return os.linesep.join(self.lines)
+        return pformat(self.sections)
 
     def __repr__(self) -> str:
-        return str(self.lines)
+        return str(self)
--- src/runmany/runner.py
+++ src/runmany/runner.py
@@ -41,8 +41,7 @@ class Placeholders:  # pylint: disable=too-few-public-methods
 def run(manyfile: str, settings: Settings) -> None:
     parser = Parser(manyfile, settings)
     # with TemporaryDirectory() as directory:
-    for x in parser:
-        print(x)
+    print(parser)
     # for section in parser.get_sections():
     #     pass
     # for also in section.get_alsos():
